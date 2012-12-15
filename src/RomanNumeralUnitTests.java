000aa98884ab61787dea3388100cb5f2c3a40710
nachum <@192.168.79.1>
2012-12-15 21:41:54 +0200

    red for 9

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -11,6 +11,13 @@ public class RomanNumeralUnitTests {
 	public void testThat1ReturnsI() {
 		assertEquals("I", number.toRoman(1));
 		assertEquals("II", number.toRoman(2));
+		assertEquals("IX", number.toRoman(9));
+//		assertEquals("MMM", number.toRoman(3000));
+//		assertEquals("MCMX", number.toRoman(1910));
+//		assertEquals("MCMLIV", number.toRoman(1954));
+//		assertEquals("MCMXC", number.toRoman(1990));
+//		assertEquals("DLXXXVII", number.toRoman(587));
+//		assertEquals("MMCCCLXXIII", number.toRoman(2373));
 	}
 	
 
